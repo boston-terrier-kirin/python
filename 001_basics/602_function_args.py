5f2c23353d4959ae71fc0b7a4aeca2b31b0f8919
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-12-07 06:53:18 +0900

    サンプル整理

--- 001_basics/602_function_args.py
+++ 001_basics/602_function_args.py
@@ -25,3 +25,46 @@ def login(first, *args):
 
 login("kirin")
 login("kirin", "kuroro")
+print("*******************")
+
+
+# *argsの後ろにcを追加できる
+def func(a, b, *args, c):
+    print(a, b, args, c)
+
+
+# これはできない
+# TypeError: func() missing 1 required keyword-only argument: 'c'
+# func(1, 2, 3, 4, 5, 6)
+
+# *argsの後ろは、namedにする必要がある
+# 1 2 (3, 4, 5) 6
+func(1, 2, 3, 4, 5, c=6)
+print("*******************")
+
+
+# *以降は、namedを強制することになる
+def func(a, b, *, c, d):
+    print(a, b, c, d)
+
+
+# TypeError: func() takes 2 positional arguments but 4 were given
+# func(1, 2, 3, 4)
+
+func(1, 2, c=3, d=4)
+print("*******************")
+
+
+# *以降は、namedを強制することになるので、これで全部がnamed
+def func(*, a, b, c, d):
+    print(a, b, c, d)
+
+
+func(a=1, b=2, c=3, d=4)
+print("*******************")
+
+
+# defaultありの後ろにpositionalはおくと、defaultを省略できなくなって困るので、シンタックスエラーになる
+# SyntaxError: non-default argument follows default argument
+# def func(a, b=1, c):
+#     print(a, b, c)
---
+++ 111_FastAPI/03_todos/TodoApp/main.py
@@ -0,0 +1,82 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
+
+import models
+from models import Todos
+from database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_deps = Annotated[Session, Depends(get_db)]
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    desc: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_deps):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_deps, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_deps, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_deps, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    todo_model.title = todo_request.title
+    todo_model.desc = todo_request.desc
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_deps, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
